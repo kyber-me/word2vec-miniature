95f2a88bdec2e0707c0ebff77ed0fc6b2f26e43d
Kyber-T <shopper.tiago@gmail.com>
2026-06-05 21:09:53 -0300

    chore: First random embedding created

--- win.py
+++ win.py
@@ -4,15 +4,24 @@ from numpy.typing import NDArray
 
 class Win:
     corpora: NDArray
+    vocabulary: NDArray | None
 
     def __init__(self, path: str = "corpora.csv") -> None:
         try:
-            # self.corpora = np.genfromtxt(path, delimiter=";")
             self.corpora = np.loadtxt(path, dtype=str, delimiter=";", skiprows=1)
         except Exception as e:
             print(e)
 
-    def build_matrix(self):
+            raise Exception("System failure")
+
+    # --- Public API ---
+
+    def build(self) -> NDArray:
+        return self._build_vocabulary()._build_matrix()
+
+    # --- Private functions ---
+
+    def _build_vocabulary(self) -> "Win":
         phrases = self.corpora[:, -1]
         # print("phrases: ", phrases)
 
@@ -29,8 +38,27 @@ class Win:
         # print("vocabulary: ", vocabulary)
 
         np.save("vocabulary.npy", vocabulary)
+        self.vocabulary = vocabulary
+
+        return self
+
+    def _build_matrix(self) -> NDArray:
+
+        if self.vocabulary is None or not len(self.vocabulary):
+            raise Exception("No vocabulary is found")
+
+        rng = np.random.default_rng()
+        out = np.array([rng.standard_normal(3) for _ in self.vocabulary])
+
+        print(f"out -> {out}")
+
+        self.embeddings = out
+
+        np.save("embeddings.npy", out)
+
+        return out
 
 
 if __name__ == "__main__":
     win = Win()
-    win.build_matrix()
+    win.build()
