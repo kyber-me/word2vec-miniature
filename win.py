a102e83057c2d83c2d6a7aee12b0167aa6a186f8
Kyber-T <shopper.tiago@gmail.com>
2026-06-10 20:54:34 -0300

    chore: bigger vocabulary created

--- win.py
+++ win.py
@@ -2,29 +2,38 @@ import numpy as np
 from numpy.typing import NDArray
 
 
-class Win:
+class WeightMatrix:
     corpora: NDArray
-    vocabulary: NDArray | None
+    vocabulary: NDArray[np.str_] | None = None
+    embeddings: NDArray[np.float64] | None = None
 
     def __init__(self, path: str = "corpora.csv") -> None:
         try:
             self.corpora = np.loadtxt(path, dtype=str, delimiter=";", skiprows=1)
+
+            self.vocabulary = np.load("vocabulary.npy", "r")
+            self.embeddings = np.load("embeddings.npy", "r")
+
         except Exception as e:
             print(e)
 
-            raise Exception("System failure")
+    def _get_phrases(self) -> NDArray:
+        return self.corpora[:, -1] if self.corpora is not None else np.array([])
 
     # --- Public API ---
 
     def build(self) -> NDArray:
-        return self._build_vocabulary()._build_matrix()
+        return (
+            self.embeddings
+            if self.embeddings
+            else self._build_vocabulary()._build_matrix()
+        )
 
     # --- Private functions ---
 
-    def _build_vocabulary(self) -> "Win":
-        phrases = self.corpora[:, -1]
-        # print("phrases: ", phrases)
+    def _build_vocabulary(self) -> "WeightMatrix":
 
+        phrases = self._get_phrases()
         vocabulary = np.array(
             sorted(
                 list(
@@ -35,7 +44,6 @@ class Win:
                 key=lambda x: x,
             )
         )
-        # print("vocabulary: ", vocabulary)
 
         np.save("vocabulary.npy", vocabulary)
         self.vocabulary = vocabulary
@@ -49,16 +57,21 @@ class Win:
 
         rng = np.random.default_rng()
         out = np.array([rng.standard_normal(3) for _ in self.vocabulary])
-
-        print(f"out -> {out}")
-
         self.embeddings = out
-
         np.save("embeddings.npy", out)
 
         return out
 
 
 if __name__ == "__main__":
-    win = Win()
-    win.build()
+    wm = WeightMatrix()
+    embeddings = wm.embeddings if wm.embeddings is not None else wm.build()
+    # if not wm.embeddings:
+    #     embeddings = wm.embeddings if wm.embeddings is not None else wm.build()
+
+    if wm.vocabulary is not None and wm.embeddings is not None:
+        # Plotar o estado inicial dos pontos no espaço vetorial semântico
+        # ...
+        for e, w in zip(wm.embeddings, wm.vocabulary):
+            print("Plot the points")
+            print(w, " - ", e)
